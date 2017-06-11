698a4acb3d94c2d8921ae01502708fdf4bca89c1
bng <bng@MPSNB040>
2017-06-11 11:41:50 +0200

    Add new instance of the inherite Class

--- Vererbung/Vererbung/main.cpp
+++ Vererbung/Vererbung/main.cpp
@@ -8,6 +8,7 @@ using namespace std;
 int main()
 {
 	CWiderstand r;
+	CWiderstand t;
 
 	r.setPosition(5, 2);
 	r.setPreis(1.99);
@@ -16,5 +17,6 @@ int main()
 	cout << "Position: (" << r.getX() << " , " << r.getY() << ")\n";
 	cout << "Preis: " << r.getPreis() << " EUR \n";
 	cout << "Widerstand: " << r.getR() << " Ohm\n";
+	cout << "Widerstand: " << r.getPreis << " EUR\n";
 	system("PAUSE");
 }
