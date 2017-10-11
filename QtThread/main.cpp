219e563ed27372805760ffeb04a943acb4df6725
bkana <berrux93@yahoo.fr>
2017-10-11 16:00:04 +0200

    thread priority

--- QtThread/main.cpp
+++ QtThread/main.cpp
@@ -6,8 +6,8 @@ int main(int argc, char *argv[])
     QCoreApplication a(argc, argv);
 
     MyThread thread1("A"), thread2("B"), thread3("C");
-    thread1.start();
+    thread1.start(QThread::LowestPriority);
     thread2.start();
-    thread3.start();
+    thread3.start(QThread::HighestPriority);
     return a.exec();
 }
