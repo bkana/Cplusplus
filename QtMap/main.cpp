5c41ba5aefa96e7878ccf9c7c8a62bca31b0bb3c
bkana <berrux93@yahoo.fr>
2017-10-11 10:28:24 +0200

    use iterator to navigate through all the key and value

--- QtMap/main.cpp
+++ QtMap/main.cpp
@@ -8,6 +8,8 @@ int main(int argc, char *argv[])
     map["one"] = 1;
     map["three"] = 3;
     map["seven"] = 7;
+    int newfive = 5;
+    map.insert("five", newfive);
 
     int num1 = map["one"];
     int num2 = map["three"];
@@ -15,5 +17,20 @@ int main(int argc, char *argv[])
     qDebug() << "nummer one are : " << num1;
     qDebug() << "nummer three are : " << num2;
     qDebug() << "nummer seven are : " << num3;
+    qDebug() << "nummer five are : " << newfive;
+
+    QMap<int , QString> Map;
+    Map.insert(1,"A");
+    Map.insert(2,"B");
+    Map[3] = "C";
+    foreach (int i, Map.keys()) {
+        qDebug() << Map[i];
+    }
+
+    QMapIterator<int, QString> iter(Map);
+    while (iter.hasNext()) {
+        iter.next();
+        qDebug() << iter.key() << " : " << iter.value();
+    }
     return a.exec();
 }
