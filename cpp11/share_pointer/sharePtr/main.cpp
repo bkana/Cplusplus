6d8540b39cfb83ba0f3b3f2383948bae90b6c964
berruxk <nkana@outlook.de>
2018-09-07 13:31:59 +0200

    basic sharePtr example

--- cpp11/share_pointer/sharePtr/main.cpp
+++ cpp11/share_pointer/sharePtr/main.cpp
@@ -10,6 +10,7 @@ private:
     string m_name;
 public:
     Dog(string name) {
+        m_name = name;
         cout << "Dog is created: " << name << endl;
     }
     Dog() {
@@ -26,10 +27,19 @@ public:
 
 void foo()
 {
-    Dog* p = new Dog("Gunner");
-    p->bark();
+    shared_ptr<Dog> p(new Dog("Gunner")); // Count = 1
 
-} // memory leak
+    {
+        shared_ptr<Dog> p2=p; // Count = 2
+        p2->bark();
+        cout << p.use_count() << endl;
+    }
+    // count = 1
+
+    p->bark();
+    cout << p.use_count() << endl;
+} // Count = 0
+
 int main()
 {
     foo();
